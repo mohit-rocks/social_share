5f853769573643d2dfcf980585cec183ac1e7b5c
Mohit Aghera <mohit.aghera@gmail.com>
2017-07-26 11:03:16 +0530

    Adding support for displaying enabled and not-enabled social media providers.

--- app/Http/Controllers/SocialAccountsController.php
+++ app/Http/Controllers/SocialAccountsController.php
@@ -11,8 +11,14 @@ class SocialAccountsController extends Controller
   protected $uid;
 
   private $accountProviders = [
-    'FacebookProvider' => 'Facebook',
-    'TwitterProvider' => 'Twitter',
+    'FacebookProvider' => [
+      'name' => 'Facebook',
+      'machine_name' => 'facebook',
+    ],
+    'TwitterProvider' => [
+      'name' => 'Twitter',
+      'machine_name' => 'twitter', // Machine name is used in generating redirect url.
+    ],
   ];
 
   public function __construct()
@@ -33,11 +39,27 @@ class SocialAccountsController extends Controller
       $connectedAccounts = SocialAccount::whereUserId($this->uid)->get();
       $users = [];
       foreach ($connectedAccounts as $account) {
-        $users[] = [
+        $users['connected'][] = [
           'provider' => $this->accountProviders[$account->provider],
           'user_name' => \Auth::user()->name,
         ];
       }
+      $users['not_connected_providers'] = $this->getDisconnectedAccounts($connectedAccounts);
       return view('accounts.index', compact('users'));
+    }
+
+  /**
+   * Retrieve service providers which are not synced yet.
+   *
+   * @param $connectedAccounts
+   *   Connected accounts to current user.
+   * @return array
+   */
+    public function getDisconnectedAccounts($connectedAccounts) {
+      $providers = $this->accountProviders;
+      foreach ($connectedAccounts as $account) {
+        unset($providers[$account->provider]);
+      }
+      return $providers;
     }
 }
--- resources/views/accounts/index.blade.php
+++ resources/views/accounts/index.blade.php
@@ -8,10 +8,19 @@
     </div>
     <div class="col-sm-8 blog-main">
         <h3>Connected Accounts:</h3>
-        @foreach($users as $account)
+        @foreach($users['connected'] as $account)
             <div class="account-data">
-                <div class="service_provider">{{ $account['provider'] }}</div>
+                <div class="service_provider">{{ $account['provider']['name'] }}</div>
                 <div class="service_provider_user">{{ $account['user_name'] }}</div>
+            </div>
+        @endforeach
+    </div>
+
+    <div class="col-sm-8 blog-main">
+        <h3>Not Connected Accounts:</h3>
+        @foreach($users['not_connected_providers'] as $key => $provider)
+            <div class="provider-data">
+                <div class="service_provider"><a href="/redirect/{{ $provider['machine_name'] }}" >{{ $provider['name'] }}</a></div>
             </div>
         @endforeach
     </div>
